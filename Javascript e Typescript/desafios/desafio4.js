97428aea6fd536d6e052d4dfa52a84f62a09badf
Junio <junioveras127@gmail.com>
2022-06-07 15:11:26 -0300

    typescript2

--- Javascript e Typescript/desafios/desafio4.js
+++ Javascript e Typescript/desafios/desafio4.js
@@ -44,7 +44,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 // Todas as requisições necessárias para as atividades acima já estão prontas, mas a implementação delas ficou pela metade (não vou dar tudo de graça).
 // Atenção para o listener do botão login-button que devolve o sessionID do usuário
 // É necessário fazer um cadastro no https://www.themoviedb.org/ e seguir a documentação do site para entender como gera uma API key https://developers.themoviedb.org/3/getting-started/introduction
-var apiKey = '3dda2982023b600b7abe8bde3864773a';
+var apiKey;
 var requestToken;
 var username;
 var password;
